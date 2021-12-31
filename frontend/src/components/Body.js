ba6b30c19acbaf2814c789ccef718df41b5b01c7
Ndèye Aïssatou GASSAMA <ayshagassama1@gmail.com>
2021-12-31 16:12:13 +0000

    Page accueil ok

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,37 +1,45 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import Spend from "./Spend";
 import Tableau from "./Tableau";
 
 const Body = () => {
 	return (
-		<div className="bloc-b-accueil">
-			<Card className="tableaux">
-				<Tableau
-					nomCategorie="Groceries"
-					classe="enteteTableauDanger"
-					depense="spent extra"
-					pastAverage="$25"
-					thisMonth="$55"
-					balance="$30"
-				/>
-				<Tableau
-					nomCategorie="Commute"
-					classe="enteteTableauSuccess"
-					depense="saved&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-					pastAverage="$55"
-					thisMonth="$25"
-					balance="$30"
-				/>
-				<Tableau
-					nomCategorie="Eating out"
-					classe="enteteTableauPrimary"
-					depense="saved&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-					pastAverage="$55"
-					thisMonth="$25"
-					balance="$30"
-				/>
-			</Card>
-		</div>
+		<>
+			<div className="bloc-a-accueil">
+				<Card>
+					<Spend />
+				</Card>
+			</div>
+			<div className="bloc-b-accueil">
+				<Card className="tableaux">
+					<Tableau
+						nomCategorie="Groceries"
+						classe="enteteTableauDanger"
+						depense="spent extra"
+						pastAverage="$25"
+						thisMonth="$55"
+						balance="$30"
+					/>
+					<Tableau
+						nomCategorie="Commute"
+						classe="enteteTableauSuccess"
+						depense="saved&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+						pastAverage="$55"
+						thisMonth="$25"
+						balance="$30"
+					/>
+					<Tableau
+						nomCategorie="Eating out"
+						classe="enteteTableauPrimary"
+						depense="saved&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+						pastAverage="$55"
+						thisMonth="$25"
+						balance="$30"
+					/>
+				</Card>
+			</div>
+		</>
 	);
 };
 
---
+++ frontend/src/components/Spend.js
@@ -0,0 +1,29 @@
+import React, { Component, useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+export default class Spend extends Component {
+	render() {
+		return (
+			<div className="u-spent">
+				<h2>You've spent</h2>
+				<div align-center className="spent-details">
+					<div>
+						<div className="my-container">
+							<img src="mai.jpg" alt="image" className="img-fluid w-50"></img>
+							<h2 className="centered">1000$</h2>
+						</div>
+					</div>
+					<div className="recent-spent">
+						<div className="bloc-spent">
+							<span className="sum-spent">$20</span> yesterday
+						</div>
+						<div className="bloc-spent">
+							<span className="sum-spent">$15</span> today
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
