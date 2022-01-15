862a735ae84ab7964dcacebf55813606b2d12fbd
Ndèye Aïssatou GASSAMA <ayshagassama1@gmail.com>
2022-01-15 16:08:17 +0000

    Frontend Globalchart ok

---
+++ frontend/src/components/GlobalChart.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+export const options = {
+	plugins: {
+		title: {
+			display: true,
+			text: "Chart.js Bar Chart - Stacked",
+		},
+	},
+	responsive: true,
+	scales: {
+		x: {
+			stacked: true,
+		},
+		y: {
+			stacked: true,
+		},
+	},
+};
+
+const labels = ["January", "February", "March", "April", "May", "June"];
+const datas = [10, 45, 60, 83, 5, 13];
+const datas1 = [71, 5, 0, 8, 51, 8];
+const datas2 = [11, 4, 40, 18, 31, 28];
+const datas3 = [31, 45, 10, 78, 41, 38];
+export const data = {
+	labels,
+	datasets: [
+		{
+			label: "Commute",
+			data: datas1,
+			backgroundColor: "rgba(54, 162, 235, 0.8)",
+		},
+		{
+			label: "Groceries",
+			data: datas,
+			backgroundColor: "rgba(75, 255, 192, 0.8)",
+		},
+		{
+			label: "Eating-out",
+			data: datas2,
+			backgroundColor: "rgba(255, 206, 86, 0.8)",
+		},
+		{
+			label: "Others",
+			data: datas3,
+			backgroundColor: "rgba(255, 99, 132, 0.8)",
+		},
+	],
+};
+
+export default function GlobalChart() {
+	return <Bar options={options} data={data} />;
+}
--- frontend/src/components/Reports.js
+++ frontend/src/components/Reports.js
@@ -1,31 +1,28 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import DailyChart from "./DailyChart";
-import { Tabs, Tab, Col, Row, Nav } from "react-bootstrap";
+import GlobalChart from "./GlobalChart";
+import { Tab, Col, Row, Nav } from "react-bootstrap";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const dailyChartData = {
-	labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
+	labels: ["Commute", "Eating-out", "Groceries", "Others"],
 	datasets: [
 		{
 			label: "# of Votes",
-			data: [12, 10, 3, 5, 2],
+			data: [12, 10, 3, 5],
 			backgroundColor: [
-				"rgba(255, 99, 132, 0.2)",
-				"rgba(54, 162, 235, 0.2)",
-				"rgba(255, 206, 86, 0.2)",
-				"rgba(75, 192, 192, 0.2)",
-				"rgba(153, 102, 255, 0.2)",
+				"rgba(54, 162, 235, 0.5)",
+				"rgba(255, 206, 86, 0.5)",
+				"rgba(75, 255, 192, 0.5)",
+				"rgba(255, 99, 132, 0.5)",
 			],
 			borderColor: [
-				"rgba(255, 99, 132, 1)",
 				"rgba(54, 162, 235, 1)",
 				"rgba(255, 206, 86, 1)",
-				"rgba(75, 192, 192, 1)",
-				"rgba(153, 102, 255, 1)",
+				"rgba(75, 255, 192, 1)",
+				"rgba(255, 99, 132, 1)",
 			],
 			borderWidth: 1,
 		},
@@ -33,8 +30,6 @@ export const dailyChartData = {
 };
 
 const Reports = () => {
-	const [key, setKey] = useState("home");
-
 	return (
 		<div className="tabCharts">
 			<Tab.Container id="left-tabs-example" defaultActiveKey="first">
@@ -51,11 +46,11 @@ const Reports = () => {
 					</Col>
 					<Col lg={9} sm={12} className="tabChartsContent">
 						<Tab.Content className="charts">
-							<Tab.Pane eventKey="first">
+							<Tab.Pane className="doughnut" eventKey="first">
 								<DailyChart chartData={dailyChartData} />
 							</Tab.Pane>
 							<Tab.Pane eventKey="second">
-								<img src="../logo.svg" alt="image1" />
+								<GlobalChart />
 							</Tab.Pane>
 						</Tab.Content>
 					</Col>
